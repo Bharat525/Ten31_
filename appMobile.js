7d302cb830d7fb5e95cee6f1f47e2bc84d259552
see.rohit525 <see.rohit525@gmail.com>
2022-01-16 14:51:43 +0530

    changes

---
+++ appMobile.js
@@ -0,0 +1,78 @@
+
+var el = document.querySelector('#imgIcon')
+console.log(el)
+
+
+// el.addEventListener('click', myFunction)
+
+
+
+
+function myFunction() {
+    var element = document.getElementById("mobile");
+    element.classList.toggle("mystyle");
+}
+
+
+
+
+console.log(elpara)
+
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+
+
+function parafun() {
+
+    var elpara = document.getElementById('para')
+    if (elpara.style.display === "none") {
+        elpara.style.display = "block";
+
+    } else {
+        elpara.style.display = "none";
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+// function myfunc() {
+//     var parent = document.getElementsByClassName('.mobile')
+
+//     if (parent.style.display === "block") {
+//         parent.style.display = "none";
+
+//     } else {
+//         parent.style.display = "block"
+//     }
+
+// }
+
+
+// function myfunc() {
+
+//     var parent = document.getElementsByClassName('.mobile')
+
+//     console.log(parent)
+
+
+
+// }
